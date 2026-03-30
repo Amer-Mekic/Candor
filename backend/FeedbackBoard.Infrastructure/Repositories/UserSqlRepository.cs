3c023b6f87894d877f577a6fb88a38eccc6023d8
Amer-Mekic <amerm3kic@gmail.com>
2026-03-31 01:02:27 +0200

    Add configs for web server, jwt. Set up user repository

---
+++ backend/FeedbackBoard.Infrastructure/Repositories/UserSqlRepository.cs
@@ -0,0 +1,47 @@
+using FeedbackBoard.Core.Entities;
+using FeedbackBoard.Core.Interfaces;
+using FeedbackBoard.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+public class UserSqlRepository : IUserRepository
+{
+    private readonly AppDbContext _context;
+
+    public UserSqlRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User?> GetByEmailAsync(string email)
+    {
+        return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+    }
+
+    public async Task AddAsync(User user)
+    {
+        await _context.Users.AddAsync(user);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<User?> GetByIdAsync(Guid id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+
+    public async Task<IReadOnlyList<User>> GetAllAsync()
+    {
+        return await _context.Users.ToListAsync(); 
+    }
+
+    public async Task UpdateAsync(User user)
+    {
+        _context.Users.Update(user);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(User user)
+    {
+        _context.Users.Remove(user);
+        await _context.SaveChangesAsync();
+    }
+}
